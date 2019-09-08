6915c8f58902d294d71490ad545203905749a8f7
MrAugu <mraugu@cksoftwares.com>
2019-09-08 13:16:09 +0000

    Smh

--- backend/config.js
+++ backend/config.js
@@ -5,8 +5,8 @@ module.exports = {
       "callbackURL": "http://localhost/callback",
       "sessionSecret": "dfd$##$%$#%$CFdfdf4%$%#@#$@@##@$#@EncryptionIsForce%^%53345#$%$#%22334+324238+294423+4e2*/1e+dsafqerq352/4+",
       "domain": "localhost",
-      "port": 80
+      "port": 3001
     },
-    "dbUrl": "mongodb://system:system~1@ds012889.mlab.com:12889/discord-house",
+    "dbUrl": "mongodb://dbh:dbhdbh1@ds012889.mlab.com:12889/discord-house",
     "admins": ["414764511489294347", "538855659714641960"]
   };
