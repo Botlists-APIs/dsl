71f604605574d1be49d1c3fe7b1836df2312e9e8
MrAugu <mraugu@yahoo.com>
2019-09-08 16:31:38 +0300

    fix

--- backend/config.js
+++ backend/config.js
@@ -2,9 +2,9 @@ module.exports = {
     "token": "NTYwODY5MTI5MzEwMTc1MjQz.XW1Dsg.A_53fEJbgJN6jce3t7sIydgECAs",
     "dashboard": {
       "oauthSecret": "Ze8JN0ENL9uFPRKMVqnBkcuxYXlbXkGs",
-      "callbackURL": "http://localhost/callback",
+      "callbackURL": "https://discordhouse.org/callback",
       "sessionSecret": "dfd$##$%$#%$CFdfdf4%$%#@#$@@##@$#@EncryptionIsForce%^%53345#$%$#%22334+324238+294423+4e2*/1e+dsafqerq352/4+",
-      "domain": "localhost",
+      "domain": "discordhouse.org",
       "port": 3001
     },
     "dbUrl": "mongodb://dbh:dbhdbh1@ds012889.mlab.com:12889/discord-house",
